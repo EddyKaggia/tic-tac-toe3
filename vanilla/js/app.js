a4066a1655abc440666e442fab7a34a411779866
EddyKaggia <kagzcodes@gmail.com>
2023-10-15 19:11:11 -0700

    Refactor from app.js to view.js and store.js

--- vanilla/js/app.js
+++ vanilla/js/app.js
@@ -1,3 +1,6 @@
+import Store from "./store.js";
+import View from "./view.js";
+
 const App = {
   // All of our selected HTML elements
   $: {
@@ -144,4 +147,72 @@ const App = {
   },
 };
 
-document.addEventListener("DOMContentLoaded", App.init);
+// document.addEventListener("DOMContentLoaded", App.init);
+
+const players = [
+  { id: 1, name: "Player 1", iconClass: "fa-x", colorClass: "turquoise" },
+  { id: 2, name: "Player 2", iconClass: "fa-o", colorClass: "yellow" },
+];
+
+function init() {
+  const view = new View();
+  const store = new Store(players);
+
+  view.bindGameResetEvent((event) => {
+    view.closeAll();
+
+    store.reset();
+
+    view.clearMoves();
+    view.setTurnIndicator(store.game.currentPlayer);
+
+    view.updateScoreboard(
+      store.stats.playerWithStats[0].wins,
+      store.stats.playerWithStats[1].wins,
+      store.stats.ties
+    );
+  });
+
+  view.bindNewRoundEvent((event) => {
+    store.newRound();
+
+    view.closeAll();
+    view.clearMoves();
+    view.setTurnIndicator(store.game.currentPlayer);
+    view.updateScoreboard(
+      store.stats.playerWithStats[0].wins,
+      store.stats.playerWithStats[1].wins,
+      store.stats.ties
+    );
+  });
+
+  view.bindPlayerMoveEvent((square) => {
+    const existingMove = store.game.moves.find(
+      (move) => move.squareId === +square.id
+    );
+
+    if (existingMove) {
+      return;
+    }
+
+    //Place an icon of the current player in a square
+    view.handlePlayerMove(square, store.game.currentPlayer);
+
+    //Advance to teh next state by pushing a move to teh moves array
+    store.playerMove(+square.id);
+
+    if (store.game.status.isComplete) {
+      view.openModal(
+        store.game.status.winner
+          ? `${store.game.status.winner.name} wins`
+          : `It's a tie!`
+      );
+      return;
+    }
+
+    //Set the next player's turn indicator
+    view.setTurnIndicator(store.game.currentPlayer);
+  });
+}
+
+window.addEventListener("load", init);
